da45f2747c96dbb9866f341f5e24dfe205055ce4
beistroff <galiasandrii@gmail.com>
2025-03-13 21:27:38 +0200

    Add ingress/egress rules and attach it to security group

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -1,29 +1,32 @@
-resource "aws_security_group" "ingress_rules" {
-  name        = "${var.network}-security-group"
-  description = "Security group for ${var.network}"
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.env}-ec2-sg"
+  description = "Security group for EC2 instances in ${var.env}"
   vpc_id      = var.vpc_id
 
-  dynamic "ingress" {
-    for_each = var.ingress_rules
-    content {
-      description = ingress.value.name
-      from_port   = ingress.value.ports[0]
-      to_port     = ingress.value.ports[0]
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-
-  # Allow all outbound traffic
+  # Ingress rules will be added dynamically
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
   }
 
   tags = {
-    Name = "${var.network}-security-group"
-    env  = var.env
+    Name        = "${var.env}-ec2-sg"
+    Environment = var.env
+    Project     = "agalias-diploma"
   }
+}
+
+resource "aws_security_group_rule" "ingress_rules" {
+  for_each = var.ingress_rules
+
+  type              = "ingress"
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  protocol          = each.value.protocol
+  cidr_blocks       = each.value.cidr_blocks
+  description       = each.value.description
+  security_group_id = aws_security_group.ec2_sg.id
 }
--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -1,27 +1,57 @@
-variable "network" {
-  description = "Network where firewall resources will be created"
-  type        = string
-}
-
-variable "ingress_rules" {
-  description = "A map of ingress rule configurations"
-  type = map(object({
-    name  = string
-    ports = list(number)
-  }))
-}
-
-variable "region" {
-  description = "AWS region"
+variable "env" {
+  description = "Environment name"
   type        = string
 }
 
 variable "vpc_id" {
-  description = "The ID of the VPC"
+  description = "VPC ID where to create security groups"
   type        = string
 }
 
-variable "env" {
-  description = "Environment name"
-  type        = string
+variable "ingress_rules" {
+  description = "Map of ingress rules for different services"
+  type = map(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = {
+    http = {
+      description = "HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    https = {
+      description = "HTTPS"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    ssh = {
+      description = "SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    backend_api = {
+      description = "Backend API"
+      from_port   = 3000
+      to_port     = 3000
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    frontend = {
+      description = "Frontend app"
+      from_port   = 4000
+      to_port     = 4000
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
 }
